fa2cb1d98da9d62c23597fa7fa60336b9c81cfa0
Jack McCambridge <jmccambridge06@qub.ac.uk>
2020-06-16 19:57:01 +0100

    Updates to add top menu and drawer functionality - prior to invalidate cache to try get the layout resources working.

--- app/src/main/java/uk/ac/qub/jmccambridge06/wetravel/MainActivity.java
+++ app/src/main/java/uk/ac/qub/jmccambridge06/wetravel/MainActivity.java
@@ -1,25 +1,71 @@
 package uk.ac.qub.jmccambridge06.wetravel;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Load the custom toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Load and prepare the drawer
+        drawer = findViewById(R.id.secondary_menu_drawer);
+        NavigationView navigationView = findViewById(R.id.secondary_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle navToggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.nav_drawer_open, R.string.nav_drawer_close);
+        drawer.addDrawerListener(navToggle);
+        navToggle.syncState();
+
+
+
+        // Load the main menu fragment for bottom navigation
         BottomNavigationView navigationBar = findViewById(R.id.main_navigation);
         navigationBar.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.main_screen_container,
+                new NewsfeedFragment()).commit();
 
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()) {
+            case R.id.view_own_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_screen_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.edit_settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.main_screen_container,
+                        new SettingsFragment()).commit();
+                break;
+            case R.id.tutorial:
+                Toast.makeText(this, "Tutorial Started", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     /**
@@ -51,4 +97,17 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+    /**
+     * If drawer is open the back button will close it otherwise back button operates normally.
+     */
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
 }
